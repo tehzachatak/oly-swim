fc1b3df238b7bdb10006a530ffe2a099a36ac9f0
Zach Ross <zach.ross@gmail.com>
2016-08-15 21:48:36 -0400

    Refined function a lot, scraping should now be one-liners

--- data-scrape.R
+++ data-scrape.R
@@ -6,15 +6,16 @@ lapply(packages, library, character.only = T)
 rm("packages")
 
 # Scrape Function (Ugly)
-ScrapeSwim <- function(year, event, distance, gender) {
+ScrapeSwim <- function(year, gender, distance, event) {
   # Scrapes swimming data from Sports-Reference to usable form
-  # e.g., 10.1 is replaced with 10.33
   #
   # Args:
-  #   x: URL to scrape
-  #   y: Cleaned object name
+  #   gender: mens or womens
+  #   event: event name
+  #   year: year of event
+  #   distance: distance of event (metres)
   # Returns:
-  #   Cleaned object
+  #   Cleaned table
   url <- paste0("http://www.sports-reference.com/olympics/summer/", year, 
                 "/SWI/", gender, "-", distance, "-metres-", event, ".html")
   '.stats_table' -> css_page
@@ -26,33 +27,28 @@ ScrapeSwim <- function(year, event, distance, gender) {
     tbl_df() -> total_table
   total_table %>>%
     filter(X1 == 'Rank') %>>% as.character -> names
+  names[7:10] <- c("gender", "year", "event", "distance")
   'Rank' %>>% grep(x = total_table$X1) -> row_of_header
-  names %>>% tolower -> names(total_table)
   (row_of_header + 1) %>>% (total_table[.:nrow(total_table),]) -> total_table
+  total_table <- total_table[1:6]
+  total_table[7] <- gender
+  total_table[8] <- year
+  total_table[9] <- event
+  total_table[10] <- distance
+  names %>>% tolower -> names(total_table)
+  return(total_table)
 }
 
 # Scrape events
-m_100m_1988_fin <- ScrapeSwim("1988", "freestyle-final", "100", "mens")
-m_100m_1988_fin <- m_100m_1988_fin[1:6]
-m_100m_1988_fin$year <- 1988
+m_100m_1988_fin <- ScrapeSwim("1988", "mens", "100", "freestyle-final")
+
 m_100m_1992_fin <- ScrapeSwim("1992", "freestyle-final", "100", "mens")
-m_100m_1992_fin <- m_100m_1992_fin[1:6]
-m_100m_1992_fin$year <- 1992
 m_100m_1996_fin <- ScrapeSwim("1996", "freestyle-final", "100", "mens")
-m_100m_1996_fin <- m_100m_1996_fin[1:6]
-m_100m_1996_fin$year <- 1996
 m_100m_2000_fin <- ScrapeSwim("2000", "freestyle-final", "100", "mens")
-m_100m_2000_fin <- m_100m_2000_fin[1:6]
-m_100m_2000_fin$year <- 2000
 m_100m_2004_fin <- ScrapeSwim("2004", "freestyle-final", "100", "mens")
-m_100m_2004_fin <- m_100m_2004_fin[1:6]
-m_100m_2004_fin$year <- 2004
 m_100m_2008_fin <- ScrapeSwim("2008", "freestyle-final", "100", "mens")
-m_100m_2008_fin <- m_100m_2008_fin[1:6]
-m_100m_2008_fin$year <- 2008
 m_100m_2012_fin <- ScrapeSwim("2012", "freestyle-final", "100", "mens")
-m_100m_2012_fin <- m_100m_2012_fin[1:6]
-m_100m_2012_fin$year <- 2012
+
 # Combine
 m_100m_fin <- rbind(m_100m_1988_fin, m_100m_1992_fin, m_100m_1996_fin, 
                     m_100m_2000_fin, m_100m_2004_fin, m_100m_2008_fin,
