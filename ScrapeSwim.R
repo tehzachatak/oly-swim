846122b80aabbe298214d492e1feaf9b6772b8dd
Zach Ross <zach.ross@gmail.com>
2016-08-16 08:59:16 -0400

    Fixed minor bug in function that caused failure on 2012 tables

--- ScrapeSwim.R
+++ ScrapeSwim.R
@@ -18,12 +18,12 @@ ScrapeSwim <- function(year, gender, distance, event) {
     html_table(header = F) %>>%
     data.frame() %>>%
     tbl_df() -> total_table
+  total_table <- total_table[1:6]
   total_table %>>%
     filter(X1 == 'Rank') %>>% as.character -> names
   names[7:10] <- c("gender", "year", "event", "distance")
   'Rank' %>>% grep(x = total_table$X1) -> row_of_header
   (row_of_header + 1) %>>% (total_table[.:nrow(total_table),]) -> total_table
-  total_table <- total_table[1:6]
   total_table[7] <- gender
   total_table[8] <- year
   total_table[9] <- event
