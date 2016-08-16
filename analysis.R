207050e3e4293cce6e5ec1f2534853f203c95442
Zach Ross <zach.ross@gmail.com>
2016-08-16 16:15:56 -0400

    Updated based on new function

--- analysis.R
+++ analysis.R
@@ -1,13 +1,11 @@
-# Scrape Sports-Reference for swimming data
+## Scrape Sports-Reference for swimming data
 
-# Environment
-rm(list=ls()) # Clear
+# Environment settings
+rm(list=ls())
 setwd("~/projects/oly-swim")
 
 # Libraries
-packages <- c('rvest','dplyr','pipeR', 'data.table', 'plyr')
-lapply(packages, library, character.only = T)
-rm("packages")
+lapply(c('rvest','dplyr','pipeR', 'data.table', 'plyr'), library, character.only = T)
 
 # Functions
 source("ScrapeSwim.R")
@@ -16,4 +14,10 @@ source("ScrapeSwim.R")
 events <- read.csv("data/events.csv")
 
 # mapply
-results <- rbindlist(mapply(ScrapeSwim, events[,1], events[,2], events[,3], events[,4], SIMPLIFY=FALSE))
+results <- rbindlist(mapply(
+  ScrapeSwim, events[,1], events[,2], events[,3], events[,4], events[,5], SIMPLIFY=FALSE))
+write.csv(results, "data/results.csv")
+
+# Analysis start
+by.swim <- results[, tally(group_by(event, distance)), athlete]
+by.swim.wide <- dcast(by.swim, athlete ~ x, value.var = "freq")
